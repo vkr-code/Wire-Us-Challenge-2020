b28454ff66d332073adc5b5a22b30e6a6991cbcf
vkr-code <60784247+vkr-code@users.noreply.github.com>
2020-09-26 00:06:26 +0530

    Update Day08.java

--- Day08.java
+++ Day08.java
@@ -1,4 +1,32 @@
-class Day8{  
-  public static void main(String[] args){
-  }
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Day8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t--!=0){
+			int n=s.nextInt();
+			HashMap<String,Integer> map=new HashMap<>();
+			for(int i=0;i<n;i++){
+				String str=s.next();
+				if(!map.containsKey(str)){
+					map.put(str, 1);
+				}else{
+					int v=map.get(str);
+					v+=1;
+					map.put(str, v);
+				}
+			}
+			int q=s.nextInt();
+			for(int i=0;i<q;i++){
+				String str=s.next();
+				System.out.println(map.get(str)==null?0:map.get(str));
+			}
+		}
+	}
+
 }
